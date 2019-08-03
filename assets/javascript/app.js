04fdf3496c561160e01747e9487a7f2309237c4a
Yantzin Cabrera <yantzincabrera@gmail.com>
2019-08-03 12:13:11 -0700

    trivia starting point

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+$('#start').on('click',function(){
+    
+})
+
+var questions =[{
+    question:"cats meow",
+    answers:["true","false"],
+    correctAnswer:"true"
+}, {
+    question:"dogs meow",
+    answers:["true","false"],
+    correctAnswer:"false"
+
+}]
+// var questions = ["cats meow","dogs meow"];
+// var answers = ["true","false"];
+
+// $(document).ready(function(){
+    // $("#start").on("click", start);
+// });
+// function start(){ 
+
+
+// }
+// $('#sub-wrapper').remove();
+/*decreasing Timer
+How many questions answered correctly
+how many questions answered incorrectly
+ */
